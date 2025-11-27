68de804c4da04db4af06b0c4e557deec436f9f1c
0xemilycodes <ylbjbnpm83@outlook.com>
2025-11-27 21:28:10 +0800

    deploy.ts

--- deploy/deploy.ts
+++ deploy/deploy.ts
@@ -5,13 +5,13 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { deployer } = await hre.getNamedAccounts();
   const { deploy } = hre.deployments;
 
-  const deployedFHECounter = await deploy("FHECounter", {
+  const deployedMaskDB = await deploy("MaskDB", {
     from: deployer,
     log: true,
   });
 
-  console.log(`FHECounter contract: `, deployedFHECounter.address);
+  console.log(`MaskDB contract: `, deployedMaskDB.address);
 };
 export default func;
-func.id = "deploy_fheCounter"; // id required to prevent reexecution
-func.tags = ["FHECounter"];
+func.id = "deploy_maskdb"; // id required to prevent reexecution
+func.tags = ["MaskDB"];
